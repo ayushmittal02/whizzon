c4e97ec31c86cf2fe5ae7bca8cabb29c0e39d164
Ayush Mittal <ayushmittal02@gmail.com>
2021-05-30 14:41:55 +0530

    changes to image input

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,11 +5,11 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className='f3'>
-                {'This magic brain will detect faces in your pictures. Give it a try.'}
+                {'Whizzon will detect faces in your pictures. Give it a try.'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input type='text' className='f4 pa2 w-70 center' onChange={onInputChange} />
+                    <input type='text' className='f4 pa2 w-70 center' placeholder="Enter Image URL" onChange={onInputChange} />
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                         onClick={onButtonSubmit}>Detect</button>
                 </div>
